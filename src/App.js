c448919e052404a6ef7aa6fcb1610180a24c0820
Umesh Rai <Umesh.Rai508@outlook.com>
2021-07-04 13:47:51 +0100

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <main>
+        <Meals></Meals>
+      </main>
     </div>
   );
 }
